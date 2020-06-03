52675af55f831a485b7e11f06d43ab6d6e0ce8ec
Axel Demborg <axel.demborg@gmail.com>
2020-06-03 14:18:54 +0200

    Updates

---
+++ precache-manifest.29f0e9b424fe5c5b3579230dcdaa90e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733f7509e12bd784a3f4cf65836b4afc",
+    "url": "/narcissus/index.html"
+  },
+  {
+    "revision": "6a37c0a36618d0f31956",
+    "url": "/narcissus/static/css/main.57299d28.chunk.css"
+  },
+  {
+    "revision": "3413cc0c0eaed9196c52",
+    "url": "/narcissus/static/js/2.cbfb0cd7.chunk.js"
+  },
+  {
+    "revision": "9f3ece4be77bc30b04966fe84f4cf2e7",
+    "url": "/narcissus/static/js/2.cbfb0cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a37c0a36618d0f31956",
+    "url": "/narcissus/static/js/main.bb95191d.chunk.js"
+  },
+  {
+    "revision": "40e12dfce597973f3049",
+    "url": "/narcissus/static/js/runtime-main.c326f0e1.js"
+  }
+]);
